3f008b50b854e48870b19aea75d2eb9bf39eb8e9
Jonazzzz <bombastickj@gmail.com>
2024-11-02 05:35:26 +0100

    docs: update playground example

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,57 @@
 <template>
   <div>
-    Nuxt module playground!
+    <h1>EasyTypeORM playground example!</h1>
+    <h2>New user</h2>
+    <input
+      v-model="newUser.name"
+      type="text"
+      placeholder="Name"
+    >
+    <input
+      v-model="newUser.email"
+      type="text"
+      placeholder="E-Mail"
+    >
+    <input
+      v-model="newUser.age"
+      type="number"
+      placeholder="Age"
+    >
+    <button
+      @click="createUser()"
+    >
+      Fetch user list
+    </button>
+
+    <h2>User list</h2>
+    <ul>
+      <li
+        v-for="user in data"
+        :key="user.id"
+      >
+        {{ user }}
+      </li>
+    </ul>
+    <button
+      @click="refresh()"
+    >
+      Fetch user list
+    </button>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+const { data, refresh } = await useFetch('/api/user/list')
+
+const newUser = ref<Omit<InstanceType<typeof User>, 'id'>>({
+  name: '',
+  email: '',
+  age: 0,
+})
+async function createUser() {
+  await $fetch('/api/user', {
+    method: 'POST',
+    body: newUser.value,
+  })
+}
 </script>
