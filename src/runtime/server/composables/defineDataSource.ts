cd8f1e51c4221b0e6ac0d786bfe57f3248077dd9
Jonazzzz <bombastickj@gmail.com>
2024-11-20 10:10:10 +0100

    refactor: optimize DataSource initialization and errors
    - Optimize DataSource initialization to handle ongoing initialization processes and improve error handling.

--- src/runtime/server/composables/defineDataSource.ts
+++ src/runtime/server/composables/defineDataSource.ts
@@ -9,15 +9,31 @@ import { DataSource, type DataSourceOptions } from 'typeorm'
 export function defineDataSource(options: DataSourceOptions) {
   const dataSource = new DataSource(options)
 
+  // This will hold the initialization promise
+  let initializing: Promise<DataSource> | null = null
+
   return async () => {
-    if (!dataSource.isInitialized) {
-      await dataSource.initialize().catch((error) => {
-        if (error instanceof Error) {
-          console.log(error.stack)
-        }
-      })
+    if (dataSource.isInitialized) {
+      return dataSource
     }
 
-    return dataSource
+    // If initialization is already in progress, wait for it
+    if (initializing) {
+      return initializing
+    }
+
+    // Start the initialization process and store the promise
+    initializing = dataSource.initialize()
+      .then(() => {
+        initializing = null
+        return dataSource
+      })
+      .catch((error) => {
+        initializing = null
+        console.error('Error during DataSource initialization:', error)
+        throw error // Rethrow to allow handling where the function is called
+      })
+
+    return initializing
   }
 }
